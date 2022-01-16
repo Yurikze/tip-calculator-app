9ea438af817458406ba48616d90635511dca2b1c
Yuri Zhulev <yuri.zhulyov@gmail.com>
2022-01-16 09:58:13 +0300

    feat: add JSX layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Page from "./components/Page/Page";
 import Header from './components/Logo/Header'
+import Main from "./components/Main/Main";
 
 
 function App() {
   return <div>
     <Page>
       <Header />
+      <Main />
     </Page>
   </div>;
 }
---
+++ src/components/Select/Select.js
@@ -0,0 +1,12 @@
+const Select = props => {
+  return (
+    <select name="tip-select">
+      <option value="5%">5%</option>
+      <option value="10%">10%</option>
+      <option value="15%">15%</option>
+      <option value="20%">20%</option>
+    </select>
+  )
+}
+
+export default Select

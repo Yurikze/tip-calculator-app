eb9f357797070e86a7b92371cbc606e9e99ed775
Yuri Zhulev <yuri.zhulyov@gmail.com>
2022-01-18 10:40:21 +0300

    feat: finish app layout

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,7 @@
 import Input from '../Input/Input';
 import classes from './Main.module.scss';
-import dollar from '../../images/icon-dollar.svg'
-import man from '../../images/icon-person.svg'
-
+import dollar from '../../images/icon-dollar.svg';
+import man from '../../images/icon-person.svg';
 
 const Main = () => {
   const tipAmoutList = [5, 10, 15, 25, 50];
@@ -33,7 +32,29 @@ const Main = () => {
           <Input type="text" id="total" icon={man} />
         </div>
       </section>
-      <section className={classes.total}>12</section>
+      <section className={classes.total}>
+        <div className={classes.total__nums}>
+          <div className={classes.total__container}>
+            <h3
+              className={`${classes.main__headline} ${classes.main__headline_color_invert} ${classes.total__heading}`}
+            >
+              Tip Amount
+            </h3>
+            <span className={classes.total__subtitle}>/ person</span>
+            <p className={classes.total__sum}>$4.27</p>
+          </div>
+          <div className={classes.total__container}>
+            <h3
+              className={`${classes.main__headline} ${classes.main__headline_color_invert} ${classes.total__heading}`}
+            >
+              Total
+            </h3>
+            <span className={classes.total__subtitle}>/ person</span>
+            <p className={classes.total__sum}>$32.27</p>
+          </div>
+        </div>
+        <button className={classes.total__reset}>Reset</button>
+      </section>
     </main>
   );
 };
